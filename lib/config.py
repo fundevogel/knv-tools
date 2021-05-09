be440138a4e1bdaa382771d4bbd36a1abe251bb3
Martin Folkers <hello@twobrain.io>
2021-05-09 22:08:32 +0200

    Load config from '$XDG_CONFIG_GOME' rc file

--- lib/config.py
+++ lib/config.py
@@ -2,29 +2,53 @@
 
 
 from configparser import SafeConfigParser
-from os.path import abspath, dirname, exists, join, realpath
+from os.path import isfile, join, realpath
+
+from xdg import xdg_config_home
+
 
 class Config(object):
     def __init__(self):
-        # Set defaults for ..
-        self.config = SafeConfigParser({
-            # .. database tasks
-            'import_dir'    : './imports',
-            'data_dir'      : './data',
-            'payment_regex' : 'Download*.CSV',
-            'order_regex'   : 'Orders_*.csv',
-            'info_regex'    : 'OrdersInfo_*.csv',
-            'invoice_file'  : 'invoices.pdf',
+        # Initialize config & provide sensible defaults
+        # TODO: This should be handled waaay more nicely ..
+        config = SafeConfigParser()
+        config['DEFAULT'] = {
+            'data_dir': './data',
+            'debug': 'off'
+        }
 
-            # .. inspector tasks
-            'match_dir'    : 'build/matches',
-            'rank_dir'     : 'build/rankings',
-            'contacts_dir' : 'build/contacts',
-        })
+        # .. database tasks
+        config['database'] = {
+            'import_dir': './imports',
+            'payment_regex': 'Download*.CSV',
+            'order_regex': 'Orders_*.csv',
+            'info_regex': 'OrdersInfo_*.csv',
+        }
+
+        # .. inspector tasks
+        config['extraction'] = {
+            'invoice_file': 'invoices.pdf',
+            'matches_dir': './build/matches',
+            'rankings_dir': './build/rankings',
+            'contacts_dir': './build/contacts',
+        }
+
+        # Load config provided by user
+        config_file = realpath(join(xdg_config_home(), 'knv-cli' 'config.ini'))
+
+        if isfile(config_file):
+            config.read(config_file)
+
+        self.config = config
 
 
     def get(self, section: str, option: str):
+        booleans = ['debug']
+
         if self.config.has_option(section, option):
+            if option in booleans:
+                return self.config.getboolean(section, option)
+
             return self.config.get(section, option)
 
         if option in self.config['DEFAULT']:
