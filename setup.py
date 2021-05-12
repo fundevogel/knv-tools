e86a0cfa8b286580d0f28399736eafdaf8dd7eac
Martin Folkers <hello@twobrain.io>
2021-05-12 15:55:46 +0200

    Expand 'setup.py'

--- setup.py
+++ setup.py
@@ -1,18 +1,37 @@
 # ~*~ coding=utf-8 ~*~
 
-from setuptools import setup
+import io
+from os.path import abspath, dirname, join
+from setuptools import find_packages, setup
+
+
+VERSION = '0.3'
+
+
+def long_description():
+    readme_file = join(dirname(abspath(__file__)), 'README.md')
+
+    with io.open(readme_file, encoding='utf8') as file:
+        return file.read()
+
 
 setup(
     name='knv-cli',
-    version='0.3',
     description='Provides tools for handling KNV data',
-    url='http://github.com/Fundevogel/knv-tools',
+    long_description=long_description(),
+    long_description_content_type='text/markdown',
+    version=VERSION,
+    license='MIT',
     author='Martin Folkers',
     author_email='hello@twobrain.io',
     maintainer='Fundevogel',
     maintainer_email='maschinenraum@fundevogel.de',
-    license='MIT',
-    py_modules=['knvcli'],
+    url='https://github.com/Fundevogel/knv-tools',
+    project_urls={
+        "Source code": "https://github.com/Fundevogel/knv-tools",
+        "Issues": "https://github.com/Fundevogel/knv-tools/issues",
+    },
+    packages=find_packages(),
     install_requires=[
         'Click',
         'pandas',
@@ -22,7 +41,7 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        knvcli=main:cli
+        knvcli=knv_cli.cli:cli
     ''',
-    zip_safe=False
+    python_requires='>=3.6',
 )
