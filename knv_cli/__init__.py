ff8dd1911ae24b7c6f1088e58aeebc24cfb23264
Martin Folkers <hello@twobrain.io>
2021-05-22 10:47:53 +0200

    Cleanup code

--- knv_cli/__init__.py
+++ knv_cli/__init__.py
@@ -1,16 +1,8 @@
-from .algorithms.contacts import get_contacts
-from .algorithms.matching import Matching
-from .algorithms.ranking import get_ranking
 from .processors.paypal import Paypal
 from .processors.shopkonfigurator import Orders, Infos
 from .utils import dedupe, group_data
 
 __all__ = [
-    # Operations
-    'Matching',
-    'get_ranking',
-    'get_contacts',
-
     # Processors
     'Paypal',
     'Orders',
