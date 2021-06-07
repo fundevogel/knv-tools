67e1d127f67cb08addca2dfa83be145762fc3132
Martin Folkers <hello@twobrain.io>
2021-06-07 16:39:05 +0200

    Add exports for new structure classes

--- knv_cli/__init__.py
+++ knv_cli/__init__.py
@@ -6,6 +6,16 @@ from .processors.knv.infos import InfoProcessor
 from .processors.knv.invoices import InvoiceProcessor
 from .processors.knv.orders import OrderProcessor
 from .processors.knv.shopkonfigurator import ShopkonfiguratorProcessor
+from .structure.abstract import Component
+from .structure.components import Molecule, Atom
+from .structure.invoices.invoice import Invoice
+from .structure.invoices.invoices import Invoices
+from .structure.orders.order import Order
+from .structure.orders.orders import Orders
+from .structure.payments.payment import Payment
+from .structure.payments.payments import Payments
+from .structure.payments.paypal import PaypalPayments
+from .structure.payments.volksbank import VolksbankPayments
 
 from .utils import build_path, dedupe, group_data, sort_data
 
@@ -14,7 +24,7 @@ __all__ = [
     # KNV Webservice API
     'Webservice',
 
-    # KNV data sources
+    # KNV data processors
     'InfoProcessor',
     'InvoiceProcessor',
     'OrderProcessor',
@@ -23,6 +33,22 @@ __all__ = [
     # Payment gateways
     'Paypal',
     'Volksbank',
+
+    # Data structures
+    # (1) Basics
+    'Component',
+    'Molecule',
+    'Atom',
+    # (2) Payments
+    'Payment',
+    'Payments',
+    'PaypalPayments',
+    'VolksbankPayments',
+    # (3) KNV data
+    'Order',
+    'Orders',
+    'Invoice',
+    'Invoices',
 
     # Utilities
     'build_path',
