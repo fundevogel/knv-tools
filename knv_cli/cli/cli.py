92d72f9151fd12bb15bbfb7275fc2cfabf31a9a9
Martin Folkers <hello@twobrain.io>
2021-06-17 18:13:46 +0200

    Add acc subtask 'show'

--- knv_cli/cli/cli.py
+++ knv_cli/cli/cli.py
@@ -64,7 +64,7 @@ def rank(config, year, quarter, months, enable_chart, limit):
     Rank sales
     '''
 
-    # Fallback to current year
+    # Fall back to current year
     if year is None: year = pendulum.today().year
 
     # Make months into list if provided
@@ -415,7 +415,7 @@ def prepare(config, year, quarter, months):
     Generate cheatsheet for accounting mode
     '''
 
-    # Fallback to current year
+    # Fall back to current year
     if year is None: year = pendulum.today().year
 
     # Make months into list if provided
@@ -465,7 +465,7 @@ def run(config, year, quarter, months):
 
     click.echo('Accounting mode ON')
 
-    # Fallback to current year
+    # Fall back to current year
     if year is None: year = pendulum.today().year
 
     # Make months into list if provided
@@ -507,7 +507,7 @@ def run(config, year, quarter, months):
 
         # Go through all unmatched payments
         # TODO: Substitute '_children'
-        for count, payment in enumerate(handler._children):
+        for index, payment in enumerate(handler._children):
             # Skip payments already marked in previous session
             if payment.assigned() or last_session.has(payment, identifier):
                 # Proceed to next payment
@@ -516,7 +516,7 @@ def run(config, year, quarter, months):
             # Wrap logic in case session gets cut short ..
             try:
                 # Print current payment identifier
-                click.echo('Payment No. {}:'.format(str(count + 1)))
+                click.echo('Payment No. {}:'.format(str(index + 1)))
                 pretty_print(payment.export())
 
                 # Declare payment hit accuracy as 'manually assigned'
@@ -656,7 +656,7 @@ def pdf(config, year, quarter, months):
     Create merged PDF invoices
     '''
 
-    # Fallback to current year
+    # Fall back to current year
     if year is None: year = pendulum.today().year
 
     # Make months into list if provided
@@ -742,6 +742,46 @@ def pdf(config, year, quarter, months):
 @acc.command()
 @pass_config
 @click.option('-y', '--year', default=None, help='Year.')
+@click.option('-m', '--month', default=None, help='Month.')
+def show(config, year, month):
+    '''
+    Show pending invoices
+    '''
+
+    # Fall back to current year
+    if year is None: year = pendulum.today().year
+
+    # Validate month
+    # (1) Convert input to integer
+    month = 0 if month is None else int(month)
+
+    # (2) Ensure validity of provided value
+    while not 1 <= month <= 12: month = click.prompt('Please enter month (1-12)', type=int)
+
+    # Initialize database
+    db = Database(config)
+
+    # Initialize invoices
+    invoices = db.get_invoices().filterBy('month', year, month).unassigned()._children
+
+    # Print
+    try:
+        for index, invoice in enumerate(invoices):
+            if invoice.is_revenue():
+                click.echo('Invoice No. {}:'.format(str(index + 1)))
+                pretty_print(invoice.export())
+
+            if not click.confirm('Show next invoice? ({} left)'.format(len(invoices) - index + 1), default=True):
+                break
+
+    except click.Abort: pass
+
+    click.echo('Exiting ..')
+
+
+@acc.command()
+@pass_config
+@click.option('-y', '--year', default=None, help='Year.')
 @click.option('-q', '--quarter', default=None, help='Quarter.')
 @click.option('-b', '--years_back', default=2, help='Years back.')
 @click.option('-c', '--enable-chart', is_flag=True, help='Create bar chart alongside results.')
@@ -750,7 +790,7 @@ def report(config, year, quarter, years_back, enable_chart):
     Generate revenue report
     '''
 
-    # Fallback to current year
+    # Fall back to current year
     if year is None: year = pendulum.today().year
 
     # Determine report period & time range
--- knv_cli/structures/invoices/invoices.py
+++ knv_cli/structures/invoices/invoices.py
@@ -1,3 +1,7 @@
+# Works with Python v3.10+
+# See https://stackoverflow.com/a/33533514
+from __future__ import annotations
+
 from operator import itemgetter
 
 from ..waypoint import Waypoint
@@ -8,8 +12,26 @@ from .revenue import Revenue
 
 class Invoices(Waypoint):
     def load(self, data: tuple) -> None:
-        invoices = data
+        invoices, = data
 
         # Build composite structure
         for item in invoices.values():
             self.add(self.invoice_types[item['Rechnungsart']](item))
+
+
+    def assigned(self) -> Invoices:
+        handler = Invoices()
+
+        for child in self._children:
+            if child.assigned(): handler.add(child)
+
+        return handler
+
+
+    def unassigned(self) -> Invoices:
+        handler = Invoices()
+
+        for child in self._children:
+            if not child.assigned(): handler.add(child)
+
+        return handler
