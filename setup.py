20913ce9b11a7be085a456bc4624e4b6544dad12
Martin Folkers <hello@twobrain.io>
2021-11-09 18:07:01 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -1,35 +1,43 @@
-import io
-from os.path import abspath, dirname, join
-from setuptools import find_packages, setup
+import setuptools
 
 
-VERSION = '0.8'
+# Load README
+with open('README.md', 'r', encoding='utf8') as file:
+    long_description = file.read()
 
 
-def long_description():
-    readme_file = join(dirname(abspath(__file__)), 'README.md')
-
-    with io.open(readme_file, encoding='utf8') as file:
-        return file.read()
-
-
-setup(
+setuptools.setup(
     name='knv-cli',
-    description='Python CLI utility and library for handling data exported from KNV & pcbis.de',
-    long_description=long_description(),
-    long_description_content_type='text/markdown',
-    version=VERSION,
-    license='MIT',
+    version='0.9.0',
     author='Martin Folkers',
     author_email='hello@twobrain.io',
     maintainer='Fundevogel',
     maintainer_email='maschinenraum@fundevogel.de',
-    url='https://github.com/Fundevogel/knv-tools',
+    description='Python CLI utility and library for handling data exported from KNV & pcbis.de',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fundevogel/knv-tools',
+    license='MIT',
     project_urls={
-        "Source code": "https://github.com/Fundevogel/knv-tools",
         "Issues": "https://github.com/Fundevogel/knv-tools/issues",
     },
-    packages=find_packages(),
+    entry_points='''
+        [console_scripts]
+        knvcli=knv_cli.cli.cli:cli
+    ''',
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Office/Business :: Financial :: Accounting",
+    ],
+    packages=setuptools.find_packages(),
     install_requires=[
         'click',
         'isbnlib',
@@ -41,9 +49,5 @@ setup(
         'xdg',
         'zeep',
     ],
-    entry_points='''
-        [console_scripts]
-        knvcli=knv_cli.cli.cli:cli
-    ''',
     python_requires='>=3.6',
 )
